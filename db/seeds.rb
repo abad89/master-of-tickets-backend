4a94e73de6e2164914100dcc571156e0f83579f6
abad89 <abadalamenti89@gmail.com>
2021-09-09 01:03:08 -0400

    add User, Event, and Ticket models and tables, and add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "User01")
+event = Event.create(name: "Cool Event", hosted_by: user.id, date: "10-01-21", time: "06:00PM")
+Ticket.create(user_id: user.id, event_id: event.id)
