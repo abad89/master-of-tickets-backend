879e5c99aaafcddffc2087d815e5fbbf45d9628e
abad89 <abadalamenti89@gmail.com>
2021-09-09 02:28:40 -0400

    add events crud routes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,45 @@
 class EventsController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+
+    def index
+        events = Event.all
+        render json: events
+    end
+
+    def show
+        event = find_event
+        render json: event
+    end
+
+    def create
+        event = Event.create(event_params)
+        render json: event, status: :created
+    end
+
+    def update
+        event = find_event
+        event.update(event_params)
+        render json: event
+    end
+
+    def destroy
+        event = find_event
+        event.destroy
+        render json: {}
+    end
+
+    private
+
+    def event_params
+        params.permit(:name, :date, :time, :hosted_by)
+    end
+
+    def find_event
+        Event.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Event not found" }, status: :not_found
+    end
+end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,26 @@ class UsersController < ApplicationController
         user = User.find_by(id: params[:id])
         render json: user
     end
+
+    def create
+        user = User.create(user_params)
+        render json: user, status: :created
+    end
+
+    # def destroy
+    #     user = User.find_by(id: params[:id])
+    #     if user
+    #         user.destroy
+    #         render json: {}
+    #     else
+    #         render json: { error: "User not found" }, status: :not_found
+    #     end
+    # end
+
+
+    private
+
+    def user_params
+        params.permit(:username)
+    end
 end
