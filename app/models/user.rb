acb46dac0e39a6750d68afb17d7967c5834c5542
abad89 <abadalamenti89@gmail.com>
2021-09-14 23:03:04 -0400

    destroy tickets and hosted events when destroying user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-    has_many :events, foreign_key: :hosted_by
-    has_many :tickets
+    validates :username, uniqueness: true
+    has_many :events, foreign_key: :hosted_by, dependent: :destroy
+    has_many :tickets, dependent: :destroy
 end
