9821f8e94a995d82a03e427d7f314028c15b41c3
abad89 <abadalamenti89@gmail.com>
2021-09-15 17:46:57 -0400

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(username: "User01")
-event = Event.create(name: "Cool Event", hosted_by: user.id, date: "10-01-21", time: "06:00PM")
-Ticket.create(user_id: user.id, event_id: event.id)
+User.destroy_all
+Event.destroy_all
+Ticket.destroy_all
+
+
+user1 = User.create(username: "User01")
+user2 = User.create(username: "Bill1984")
+user3 = User.create(username: "AwesomeUser33")
+user4 = User.create(username: "BestUsernameEver")
+
+
+event1 = Event.create(name: "Cool Event", hosted_by: user1.id, date: "10-01-21", time: "06:00PM")
+event2 = Event.create(name: "Bake Sale", hosted_by: user2.id, date: "09-20-21", time: "04:00PM")
+event3 = Event.create(name: "House Party", hosted_by: user3.id, date: "10-05-21", time: "07:00PM")
+
+
+
+Ticket.create(user_id: user2.id, event_id: event1.id)
+Ticket.create(user_id: user1.id, event_id: event2.id)
+Ticket.create(user_id: user3.id, event_id: event1.id)
+Ticket.create(user_id: user4.id, event_id: event2.id)
